2923a7e508e1cc3c2789e1214324033e7cb083ca
Damian Z <damian9203@gmail.com>
2022-03-31 15:28:07 +0200

    Revision 6.0.2

---
+++ BudgetPlannerMVC.Web/Controllers/AmountController.cs
@@ -0,0 +1,89 @@
+﻿using BudgetPlannerMVC.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlannerMVC.Web.Controllers
+{
+    public class AmountController : Controller
+    {
+        private static IList<AmountModel> amounts = new List<AmountModel>();
+        // GET: AmountController
+        public ActionResult Index()
+        {
+            return View(amounts);
+        }
+
+        // GET: AmountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AmountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AmountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AmountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AmountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AmountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AmountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BudgetPlannerMVC.Web/Models/AmountModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlannerMVC.Web.Models
+{
+    public class AmountModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Type name")]
+        public string TypeName { get; set; }
+        [DisplayName("Type of amount")]
+        public TypeOfAmount TypeOfAmount { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+    }
+}
