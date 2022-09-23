d9db6da593e8ebf3769fc76f28e0e24862b3d5fe
Damian Z <damian9203@gmail.com>
2022-09-23 15:20:27 +0200

    Revision 10.0.1 Configure Startup

---
+++ BudgetPlannerAPI/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BudgetPlannerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel loginModel)
+        {
+            IActionResult response = Unauthorized();
+            var success = AuthenticateUser(loginModel);
+
+            if (success)
+            {
+                var tokenString = GenerateJsonWebToken(loginModel);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private object GenerateJsonWebToken(object loginModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool AuthenticateUser(UserModel loginModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BudgetPlannerAPI/Startup.cs
+++ BudgetPlannerAPI/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,11 +7,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using BudgetPlannerMVC.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 
 namespace BudgetPlannerAPI
 {
@@ -26,6 +31,29 @@ namespace BudgetPlannerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<Context>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -47,6 +75,8 @@ namespace BudgetPlannerAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
