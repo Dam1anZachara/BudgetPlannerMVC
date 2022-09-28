cfd4ba61b07b8bb493d677979286016ec6238bb1
Damian Z <damian9203@gmail.com>
2022-09-28 23:49:12 +0200

    Revision 10.0.4 Added BudgetStatusController

---
+++ BudgetPlannerAPI/Controllers/BudgetStatusController.cs
@@ -0,0 +1,30 @@
+﻿using BudgetPlannerMVC.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetPlannerAPI.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetStatusController : ControllerBase
+    {
+        private readonly IBudgetStatusService _budgetStatusService;
+        public BudgetStatusController(IBudgetStatusService budgetStatusService)
+        {
+            _budgetStatusService = budgetStatusService;
+        }
+        [HttpGet]
+        //[Authorize(Roles = "Admin, User")]
+        public IActionResult Get()
+        {
+            var plan = _budgetStatusService.GetActivePlanToBudgetStatusVm();
+            var planTypes = _budgetStatusService.GetPlanTypesOfPlanForBudgetStatusVm(plan);
+            var amounts = _budgetStatusService.GetAmountsOutOfPlan(plan, planTypes);
+            var sumValues = _budgetStatusService.GetSumValuesForBudgetStatusVm(planTypes, amounts);
+            var model = _budgetStatusService.GetBudgetStatusForVm(planTypes, sumValues, plan);
+            return Ok(model);
+        }
+    }
+}
--- BudgetPlannerAPI/Controllers/BudgetUserController.cs
+++ BudgetPlannerAPI/Controllers/BudgetUserController.cs
@@ -1,5 +1,6 @@
 ﻿using BudgetPlannerMVC.Application.Interfaces;
 using BudgetPlannerMVC.Application.ViewModels.BudgetUserView;
+using BudgetPlannerMVC.Application.ViewModels.UserRoleView;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -45,6 +46,13 @@ namespace BudgetPlannerAPI.Controllers
             model.CurrentUserId = currentUserId;
             return Ok(model);
         }
+        [HttpGet("{id}")]
+        //[Authorize(Roles = "Admin, User, PreUser")]
+        public IActionResult Details(int id)
+        {
+            var budgetUser = _budgetUserService.GetBudgetUserForEdit(id);
+            return Ok(budgetUser);
+        }
         [HttpPut]
         //[ValidateAntiForgeryToken]
         //[Authorize(Roles = "PreUser")]
@@ -65,6 +73,33 @@ namespace BudgetPlannerAPI.Controllers
             if (User.IsInRole("Admin") || (User.IsInRole("User") && model.UserId == currentUserId))
             {
                 _budgetUserService.UpdateBudgetUser(model);
+                return NoContent();
+            }
+            return BadRequest();
+        }
+        [Route("Role/Edit")]
+        [HttpPut]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> EditUserRoleAsync(EditUserRoleVm editUserRoleVm)
+        {
+            _userRoleService.UpdateUserRole(editUserRoleVm.UserId, editUserRoleVm.UserRoleId);
+            if (editUserRoleVm.UserRoleId == "Admin")
+            {
+                await _signInManager.SignOutAsync();
+            }
+            return NoContent();
+        }
+        [HttpDelete]
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var budgetUser = _budgetUserService.GetBudgetUserForEdit(id);
+            var user = await _userManager.FindByIdAsync(budgetUser.UserId);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                _budgetUserService.DeleteBudgetUser(id);
                 return NoContent();
             }
             return BadRequest();
